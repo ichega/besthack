069b1ddcf56e108dfecda141f332bf62495cc9be
nikita <bomberman192@yandex.ru>
2019-03-30 17:49:58 +0300

    start REST API

--- bh_platform/core/urls.py
+++ bh_platform/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index_view
+from .views import index_view, get_events
 
 urlpatterns = [
     path('', index_view),
+    path('get_events/', get_events),
 ]
--- bh_platform/core/views.py
+++ bh_platform/core/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 
 # Create your views here.
 
+
+def get_events(request):
+    
+    return JsonResponse({"Hello":"World"})
 
 def index_view(request):
     print("Hello")
